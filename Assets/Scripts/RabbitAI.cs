b933be9dab1d84a736d88625f41dd73eb41f0da1
Jishnu Girish <jishnugirish@gmail.com>
2018-04-01 17:54:13 -0400

    Basic subsumption architechture and movement

---
+++ Assets/Scripts/RabbitAI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitAI : MonoBehaviour {
+
+	enum RabbitState { Move, Mine, Attack};
+	static int priorityValue = 0;				// default value 0 is highest priority
+	List<int> queuedLayers = new List<int>();
+	Rigidbody2D rBody;
+	RabbitProperties properties;
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody2D>();
+		properties = GetComponent<RabbitProperties>();
+		queuedLayers.Add((int)RabbitState.Move);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		try
+		{
+			if (queuedLayers == null || queuedLayers.Count == 0)
+			{
+				queuedLayers.Add(0);
+			}
+			else
+			{
+				queuedLayers.Sort();
+				if (queuedLayers[0] == (int)RabbitState.Move)
+				{
+					// Call move function
+					Move();
+				}
+				else if (queuedLayers[0] == (int)RabbitState.Mine)
+				{
+					// Call mine function
+					//Mine();
+				}
+				else if (queuedLayers[0] == (int)RabbitState.Attack)
+				{
+					// Call attack function
+					//Attack();
+				}
+			}
+		}
+		catch (System.IndexOutOfRangeException e)
+		{
+			throw new System.ArgumentOutOfRangeException("index parameter is out of range.", e);
+		}
+	}
+
+	public void Move()
+	{
+		if (properties.onFloor)
+		{
+			transform.position += new Vector3(properties.speed * Time.deltaTime, 0, 0);
+		}
+		else if (!properties.onFloor)
+		{
+			transform.position += new Vector3(0, -properties.gravity * Time.deltaTime, 0);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Floor")
+		{
+			properties.onFloor = true;
+			rBody.gravityScale = 0;
+		}
+		else
+		{
+			properties.onFloor = false;
+			rBody.gravityScale = 1;
+		}
+	}
+}
+
---
+++ Assets/Scripts/RabbitProperties.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitProperties : MonoBehaviour {
+	[Range(0,100)]
+	public float speed;
+	[Range(0, 10)]
+	public float gravity;
+	public int currentState = 0;
+	public bool onFloor = false;
+
+}
