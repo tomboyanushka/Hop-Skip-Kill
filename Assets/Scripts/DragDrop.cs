688a89ba804f16f030c542aadbea6139a5288d73
kaustabhdutta <kaustabh.dutta@gmail.com>
2018-04-02 17:35:34 -0400

    Menu

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*public class DragDrop : MonoBehaviour, IBeginRabbitDragDrop, IRabbitDragDrop, IEndDragDrop {
+
+    #region IBeginRabbitDragDrop implementation
+
+    public void OnRabbitDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #endregion
+
+    #region IRabbitDragDrop implementation
+
+    public void OnMouseDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+
+    #region IEndDragDrop implementation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}*/
--- Assets/Scripts/MetaAI.cs
+++ Assets/Scripts/MetaAI.cs
@@ -35,5 +35,5 @@ public class MetaAI : MonoBehaviour
         
     }
 
-
+    
 }
