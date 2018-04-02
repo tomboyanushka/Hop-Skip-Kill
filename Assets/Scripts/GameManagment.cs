5210e1e8eb77803f2ccdf6472ea6c76cb97d60bc
Ruoyao Wu <36686782+DavidWry@users.noreply.github.com>
2018-04-02 17:26:44 -0400

    GameManager
    There is a really simple game manager. Put all managements here.

---
+++ Assets/Scripts/GameManagment.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagment : MonoBehaviour {
+
+    public int PlayerResourceA;
+    public int EnemyResourceA;
+    public int PlayerResourceB;
+    public int EnemyResourceB;
+    public int PlayerResourceC; //C for stolen resources.
+    public int EnemyResourceC; 
+
+    int playerResourceSum;
+    int enemyResourceSum;
+
+    int goal;
+
+    // Use this for initialization
+    void Start () {
+        PlayerResourceA = 0;
+        PlayerResourceB = 0;
+        PlayerResourceC = 0;
+        playerResourceSum = 0;
+        EnemyResourceA = 0;
+        EnemyResourceB = 0;
+        EnemyResourceC = 0;
+        enemyResourceSum = 0;
+
+        goal = 50;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerResourceSum = PlayerResourceA + PlayerResourceB + PlayerResourceC;
+        enemyResourceSum = EnemyResourceA + EnemyResourceB + EnemyResourceC;
+
+        if(playerResourceSum >= goal)
+        {
+            Debug.Log("Player Win!");
+        }
+
+        if(enemyResourceSum >= goal)
+        {
+            Debug.Log("Enenmy Win!");
+        }
+	}
+}
