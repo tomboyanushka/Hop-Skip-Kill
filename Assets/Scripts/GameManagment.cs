ed9134798101bc90d8379646084a35513801263b
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-04-02 22:47:20 -0400

    something

--- Assets/Scripts/GameManagment.cs
+++ Assets/Scripts/GameManagment.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManagment : MonoBehaviour {
+public class GameManagment : MonoBehaviour
+{
 
     public int PlayerResourceA;
     public int EnemyResourceA;
