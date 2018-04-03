a3c408aca2a40a62c0d84aae1fa35dc6c84da80b
kaustabhdutta <kaustabh.dutta@gmail.com>
2018-04-02 20:47:48 -0400

    Drag Drop update

---
+++ Assets/Scripts/Slots.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*public class Slots : MonoBehaviour, IDropHandler {
+    
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0);
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragDrop.dragRabbit.transform.SetParent(transform);
+        }
+    }
+}*/
